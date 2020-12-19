0276e6effd12f7bc50d157ab3f4275a10e08fc5c
‘Juliano <julianoperins@gmail.com>
2020-12-18 21:40:47 -0500

    Fetched the data and displayed on the main page

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,29 @@
 console.log("Worked!");
 
-// const input = document.querySelector(".input");
-// const btn = document.querySelector(".btn");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-// console.log(input.target.value);
+const searchTerm = document.querySelector("h2");
+const place = document.querySelector("h3");
+const weather = document.querySelector("h4");
 
-fetch("http://localhost:3000/weather?address=union")
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+        place.innerText = data.location;
+        weather.innerText = data.forecast;
+      }
+    });
+  console.log(location);
+
+  searchTerm.innerText = `Search Term "${location}"`;
+});
